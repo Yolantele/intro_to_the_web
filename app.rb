b7b8db24a1aefda1ef4e3074e0cb26f96e54e7d7
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2017-11-06 17:08:54 +0000

    added css described html image

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+# set :session_secret, 'super secret'
 
   get '/' do
     "Hello World"
@@ -18,4 +19,9 @@ require 'sinatra'
 
   get '/upcoming_news' do
     'upcoming news and cool stuff'
+  end
+
+  get '/cat' do
+    '<img style="-webkit-user-select: none;background-position: 10px 10px, 10px 10px;background-size: 20px 20px;border: medium dashed red;background-image:linear-gradient(45deg, #eee 25%, transparent 25%, transparent 50%, #eee 75%, #eee 100%),linear-gradient(45deg, #eee 25%, white 25%, white 75%, #eee 75%, #eee 100%);" src="http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png">'
+
   end
