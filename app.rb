67ee200ede7fd11030b34fb96edeac8468556781
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2017-11-07 11:37:45 +0000

    separating view from logic html

--- app.rb
+++ app.rb
@@ -21,7 +21,12 @@ require 'sinatra'
     'upcoming news and cool stuff'
   end
 
-  get '/cat' do
-    '<img style="-webkit-user-select: none;background-position: 10px 10px, 10px 10px;background-size: 20px 20px;border: medium dashed red;background-image:linear-gradient(45deg, #eee 25%, transparent 25%, transparent 50%, #eee 75%, #eee 100%),linear-gradient(45deg, #eee 25%, white 25%, white 75%, #eee 75%, #eee 100%);" src="http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png">'
+  get '/random-cat' do
+    @name = ["Amigo", "Oscar", "Viking"].sample
+    erb(:index)
+  end
 
+  get '/named-cat' do
+    @name = params[:name]
+    erb(:index)
   end
